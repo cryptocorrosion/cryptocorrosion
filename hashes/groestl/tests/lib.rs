80cafffc0b68d9b45530c781700868fb83ac46fb
Kaz Wesley <keziahw@gmail.com>
2017-12-03 15:10:57 -0800

    more tests

--- hashes/groestl/tests/lib.rs
+++ hashes/groestl/tests/lib.rs
@@ -6,8 +6,37 @@ extern crate digest;
 use digest::dev::{main_test, Test};
 
 #[test]
-fn groestl_256() {
-    let tests = new_tests!("groestl_256/test32_0", "groestl_256/test32_17",
-                          "groestl_256/test32_64", "groestl_256/test32_123");
+fn groestl_256_0() {
+    let tests = new_tests!("groestl_256/test32_0");
+    main_test::<groestl_aesni::Groestl256>(&tests);
+}
+
+#[test]
+fn groestl_256_17() {
+    let tests = new_tests!("groestl_256/test32_17");
+    main_test::<groestl_aesni::Groestl256>(&tests);
+}
+
+#[test]
+fn groestl_256_32() {
+    let tests = new_tests!("groestl_256/test32_32");
+    main_test::<groestl_aesni::Groestl256>(&tests);
+}
+
+#[test]
+fn groestl_256_64() {
+    let tests = new_tests!("groestl_256/test32_64");
+    main_test::<groestl_aesni::Groestl256>(&tests);
+}
+
+#[test]
+fn groestl_256_123() {
+    let tests = new_tests!("groestl_256/test32_123");
+    main_test::<groestl_aesni::Groestl256>(&tests);
+}
+
+#[test]
+fn groestl_256_131() {
+    let tests = new_tests!("groestl_256/test32_131");
     main_test::<groestl_aesni::Groestl256>(&tests);
 }
