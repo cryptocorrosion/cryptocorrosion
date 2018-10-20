e759f090161847147023aabe6193f94e9a411e29
Kaz Wesley <kaz@lambdaverse.org>
2018-10-20 10:24:03 -0700

    simplify testing

--- hashes/groestl/tests/lib.rs
+++ hashes/groestl/tests/lib.rs
@@ -1,42 +1,22 @@
-#![no_std]
-extern crate groestl_aesni;
-#[macro_use]
-extern crate digest;
+use digest::Digest;
 
-use digest::dev::{main_test, Test};
+fn do_test(path: &str) {
+    let input = std::fs::read(format!("tests/data/{}.input.bin", path)).unwrap();
+    let output = std::fs::read(format!("tests/data/{}.output.bin", path)).unwrap();
+    let hash = groestl_aesni::Groestl256::digest(&input);
+    assert_eq!(&hash[..], &output[..]);
+}
 
 #[test]
 fn groestl_256_0() {
-    let tests = new_tests!("groestl_256/test32_0");
-    main_test::<groestl_aesni::Groestl256>(&tests);
-}
-
-#[test]
-fn groestl_256_17() {
-    let tests = new_tests!("groestl_256/test32_17");
-    main_test::<groestl_aesni::Groestl256>(&tests);
-}
-
-#[test]
-fn groestl_256_32() {
-    let tests = new_tests!("groestl_256/test32_32");
-    main_test::<groestl_aesni::Groestl256>(&tests);
-}
-
-#[test]
-fn groestl_256_64() {
-    let tests = new_tests!("groestl_256/test32_64");
-    main_test::<groestl_aesni::Groestl256>(&tests);
-}
-
-#[test]
-fn groestl_256_123() {
-    let tests = new_tests!("groestl_256/test32_123");
-    main_test::<groestl_aesni::Groestl256>(&tests);
-}
-
-#[test]
-fn groestl_256_131() {
-    let tests = new_tests!("groestl_256/test32_131");
-    main_test::<groestl_aesni::Groestl256>(&tests);
+    for path in &[
+        "groestl_256/test32_0",
+        "groestl_256/test32_17",
+        "groestl_256/test32_32",
+        "groestl_256/test32_64",
+        "groestl_256/test32_123",
+        "groestl_256/test32_131",
+    ] {
+        do_test(path);
+    }
 }
