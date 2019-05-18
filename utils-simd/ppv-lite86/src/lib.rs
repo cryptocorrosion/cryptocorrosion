1c18224c023d2eca6adb5fc16d22e5095037b2db
Kaz Wesley <kaz@lambdaverse.org>
2019-05-18 15:13:13 -0700

    test build under emscripten

--- utils-simd/ppv-lite86/src/lib.rs
+++ utils-simd/ppv-lite86/src/lib.rs
@@ -9,14 +9,32 @@ mod soft;
 mod types;
 pub use self::types::*;
 
-#[cfg(all(feature = "simd", target_arch = "x86_64", not(miri)))]
+#[cfg(all(
+    feature = "simd",
+    target_arch = "x86_64",
+    not(miri),
+    not(target_os = "emscripten")
+))]
 pub mod x86_64;
-#[cfg(all(feature = "simd", target_arch = "x86_64", not(miri)))]
+#[cfg(all(
+    feature = "simd",
+    target_arch = "x86_64",
+    not(miri),
+    not(target_os = "emscripten")
+))]
 use self::x86_64 as arch;
 
-#[cfg(any(miri, not(all(feature = "simd", any(target_arch = "x86_64")))))]
+#[cfg(any(
+    miri,
+    target_os = "emscripten",
+    not(all(feature = "simd", any(target_arch = "x86_64")))
+))]
 pub mod generic;
-#[cfg(any(miri, not(all(feature = "simd", any(target_arch = "x86_64")))))]
+#[cfg(any(
+    miri,
+    target_os = "emscripten",
+    not(all(feature = "simd", any(target_arch = "x86_64")))
+))]
 use self::generic as arch;
 
 pub use self::arch::{vec128_storage, vec256_storage, vec512_storage};
