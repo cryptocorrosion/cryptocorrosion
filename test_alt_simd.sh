2750776d287f8459129c3e22ac6d0f2a5452cf9b
Kaz Wesley <kaz@lambdaverse.org>
2020-09-18 14:53:03 -0700

    enable simd by default
    Correct features tests to fit the feature changes from 785bfbe
    
    fixes #35

--- test_alt_simd.sh
+++ test_alt_simd.sh
@@ -9,12 +9,10 @@ if [ -n "$FAILFAST" ]; then set -e; fi
 # not ported to crypto-simd API yet:
 # - hashes/groestl
 
-echo BACKEND ppv-null
-cd hashes/blake; cargo test --no-default-features; cd ../..
-cd hashes/jh; cargo test --no-default-features; cd ../..
-cd stream-ciphers/chacha; cargo test --no-default-features; cd ../..
+echo GENERIC
+pushd stream-ciphers/chacha; cargo test --features no_simd; popd
 
-echo BACKEND packed_simd
-cd hashes/blake; cargo test --no-default-features --features packed_simd,std; cd ../..
-cd hashes/jh; cargo test -p jh-x86_64 --no-default-features --features packed_simd,std; cd ../..
-cd stream-ciphers/chacha; cargo test --no-default-features --features packed_simd,std; cd ../..
+#echo BACKEND packed_simd
+#cd hashes/blake; cargo test --no-default-features --features packed_simd,std; cd ../..
+#cd hashes/jh; cargo test -p jh-x86_64 --no-default-features --features packed_simd,std; cd ../..
+#cd stream-ciphers/chacha; cargo test --no-default-features --features packed_simd,std; cd ../..
